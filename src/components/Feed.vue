90320f30113636693d837f702954fbf24bd9d498
surajung <hdoc79@gmail.com>
2017-09-27 21:20:48 +0900

    first commit

---
+++ src/components/Feed.vue
@@ -0,0 +1,20 @@
+<template>
+  <md-card>
+    <md-card-media>
+      <img :src="imageUrl" alt="">
+    </md-card-media>
+    <md-card-header>
+      <div class="md-title">{{ fullName }}</div>
+      <div class="md-subhead">@{{ userName }}</div>
+    </md-card-header>
+    <md-card-content>
+      {{ contents }}
+    </md-card-content>
+  </md-card>
+</template>
+<script>
+export default {
+  name: 'Feed',
+  props: ['imageUrl', 'fullName', 'userName', 'contents']
+}
+</script>
---
+++ src/components/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <md-button class="md-raised md-primary"
+      href="https://www.instagram.com/oauth/authorize/?client_id=6c5bcfe280a74640a0123d7dc5d2d0cf&redirect_uri=http://localhost:8080&response_type=token&scope=public_content">
+      인스타그램 로그인
+    </md-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login'
+  }
+</script>
